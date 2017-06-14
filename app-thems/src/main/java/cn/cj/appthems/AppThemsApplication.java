07ee7b2d01c2c3c50d906deb776af650e3c2df24
chenj <chen_j126@126.com>
2017-06-15 00:00:02 +0800

    create app-thems

---
+++ app-thems/src/main/java/cn/cj/appthems/AppThemsApplication.java
@@ -0,0 +1,12 @@
+package cn.cj.appthems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppThemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppThemsApplication.class, args);
+	}
+}
---
+++ app-thems/src/test/java/cn/cj/appthems/AppThemsApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cj.appthems;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppThemsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
