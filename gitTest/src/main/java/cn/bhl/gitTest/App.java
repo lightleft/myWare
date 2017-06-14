54b63ca3f2833183dcdc3d9bd1dacc8a4f084114
lightleft <chen_j126@126.com>
2017-06-14 17:30:56 +0800

    hello create

--- gitTest/src/main/java/cn/bhl/gitTest/App.java
+++ gitTest/src/main/java/cn/bhl/gitTest/App.java
@@ -1,13 +1,13 @@
 package cn.bhl.gitTest;
 
+import cn.bhl.gitTest.core.Start;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		new Start().run();
+	}
 }
---
+++ gitTest/src/main/java/cn/bhl/gitTest/core/Start.java
@@ -0,0 +1,7 @@
+package cn.bhl.gitTest.core;
+
+public class Start {
+	public void run(){
+		System.out.println("start...go");
+	}
+}
