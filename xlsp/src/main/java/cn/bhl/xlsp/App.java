685d1133919a6d2bb256034435a960173ac567f9
lightleft <chen_j126@126.com>
2017-06-15 10:15:08 +0800

    xlsp create

---
+++ xlsp/src/main/java/cn/bhl/xlsp/App.java
@@ -0,0 +1,13 @@
+package cn.bhl.xlsp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
